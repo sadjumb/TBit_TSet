17baec868d0b18ba3d5d5964ba1d643a1cb24d54
Kirill Makarovsky <kirill-ksm@yandex.ru>
2021-09-27 18:31:30 +0300

    Update TBitField.cpp

--- TBitField.cpp
+++ TBitField.cpp
@@ -97,7 +97,7 @@ std::string TBitField::TBitToStr(int sizeU) const
 	int k;
 	for(size_t i = 0; i < size; ++i)
 	{
-		for(size_t j = 0; j < sizeof(int) * 8; ++j)
+		for(size_t j = 0; j < sizeof(unsigned int) * 8; ++j)
 		{
 			if((mem[i] & (1 << j)) > 0)
 			{
